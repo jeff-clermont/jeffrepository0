96604951bf150f4b4b7e218df9e840f1d59c2610
jeff-clermont <jeffreyclermont10@gmail.com>
2025-09-12 19:29:25 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -8,17 +8,17 @@ output "yearofarrival" {
   value=var.year-arrival
 }
 output "montrealrfancyestaurant1" {
-  value=var.restaurant-montreal1
+  value=var.fancy-restaurant1
 }
 output "montrealfancyrestaurants2" {
-  value=var.restaurant-montreal2
+  value=var.fancy-restaurant2
 }
-output "montrealfastfoodrestaurants1" {
-  value=var.restaurant-montreal3
+output "montrealfancyrestaurants2" {
+  value=var.fastfood-montreal1
 }
 output "montrealfastfoodrestaurants2" {
-  value=var.restaurant-montreal4
+  value=var.fastfood-montreal2
 }
 output "montrealfastfoodrestaurants3" {
-  value=var.restaurant-montreal5
+  value=var.fastfood-montreal3
 }
